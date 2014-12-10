1e3117a52938d62c9a2c9892e2d6567a3d1f950a
alumno335gcb16 <ale.tru.lpz@gmail.com>
2014-12-10 12:33:13 -0600

    exam3
    subida2

---
+++ Untitled-3.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Outfits</title>
+<style type="text/css">
+body {
+	background-color: #F66;
+}
+</style>
+</head>
+
+<body>
+<h1 align="center"><font face="Hobo Std"> Moda: 10 Outfits para este Otoño-Invierno </font> </h1>
+
+<p><img src="file:///C|/wamp/www/sitio/imagenes/Moda_30_Outfits_para_este_Oto_o_Invierno_1.jpg" align="bottom"></p>
+<p> <font face="Chaparral Pro Light" size="5"> <strong>Como toda mujer a veces es difícil vestirnos en época de fríos y escoger buenos outfits. Hoy en Estilizadas les daremos como a modo de orientación 30 outfits para el Otoño-Invierno. Con la ayuda de los outfits podemos lograr grandes tenidas para el día a día. Les mostraremos outfits de moda con un look mas formal y otros looks mas casuales, obviamente que cada outfits depende del lugar al cual vamos asistir.</strong></font></p>
+  
+  
+</body>
+</html>
---
+++ js/acciones.js
@@ -0,0 +1,65 @@
+// JavaScript Document
+
+
+$(document).ready(function(e) { 
+// watchId se refiere a la aceleracion 'actual'
+// 
+var watchID = null;
+document.addEventListener("deviceready", Dispositivo_Listo, false);
+
+
+//Cuando esta listo el dispositivo
+//
+function Dispositivo_Listo() {
+	Comienza();
+}
+//Empieza la 'observacion' de la aceleracion
+//
+function Comienza() {
+	//Actualiza la aceleracion cada 2 segundos
+	//
+	var opciones = { frequency: 2000 };
+	watchID = navigator.accelerometer.watchAcceleration(Correcto, Error, opciones); navigator.geolocation.getCurrentPosition(Localiza, ErrorLocalizacion);
+}
+//Detiene la 'observacion' de la aceleracion 
+//
+function Detente() { 
+if (watchID) {
+	navigator.accelerometer.clearWatch(watchID);
+	watchID= null;
+   }
+}
+ // Correcto: Toma una captura de la aceleracion
+ //
+ function Correcto(acceleration) { 
+ var element = document.getElementById('acelerometro');
+ element.innerHTML = 'Aceleracion en X: ' + acceleration.x + '<br />' +
+                     'Aceleracion en Y: ' + acceleration.y + '<br />' +
+					 'Aceleracion en Z: ' + acceleration.z + '<br />' +
+                     'Intervalo: '   + acceleration.timestamp + '<br />';
+ }
+ //error: Falla al obtener aceleracion 
+ //
+ function Error() { 
+ alert('Error!');
+ }
+ //Exito al localizar
+ //
+ function Localiza(posicion) {
+	 var element = document.getElementById('geolocalizacion');
+	 element.innerHTML = 'Latitud: '        + posicion.coord.latitude    + '<br />' +
+	                     'Longitud: '       + posicion.coord.longitude   + '<br />' +
+						 'Altitud: '        + posicion.coord.altitude    + '<br />' +
+						 'Precision: '        + posicion.coord.accuracy    + '<br />' +
+						 'Precision de Altitud: '        + posicion.coord.altitudeAccuracy    + '<br />' +
+						 'Direccion: '        + posicion.coord.heading    + '<br />' +
+						 'Velocidad:'        + posicion.coord.speed    + '<br />' +
+						 'Intervalo:'        + posicion.timestamp       + '<br />';
+ }
+ //Error en la geolocalizacion
+ //
+ function ErrorLocalizacion(error) {
+	 alert('codigo:'   + error.code  + '\n' + 
+	        'mendaje: ' + error.message +'\n');
+ }
+});//documento ready
